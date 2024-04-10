18ef60796f6ef04cd8da4fc37f4b0e144e41555b
josephcheel <josephcheelnakashima@gmail.com>
2024-04-10 14:57:13 +0200

    some cleaning and mariadb Dockefile not working

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -3,7 +3,8 @@
 if [ -d "/var/www/wordpress/wp-admin" ]; then
 	 echo "WordPress core is already downloaded"
 else
-	wp core download --path=wordpress/ --allow-root
+	echo "WordPress Installing"
+	wp core download --path=/var/www/wordpress --allow-root
 fi
 
 /usr/sbin/php-fpm7.4 -F
