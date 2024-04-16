1558c5d47debc21e43c19227ac7051599f15656b
josephcheel <josephcheelnakashima@gmail.com>
2024-04-17 00:42:33 +0200

    Apparently Mairadb is configuring automatically

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -5,6 +5,11 @@ if [ -d "/var/www/wordpress/wp-admin" ]; then
 else
 	echo "WordPress Installing"
 	wp core download --path=/var/www/wordpress --allow-root
+	# wp config DB_NAME $DB_NAME
+	# wp config DB_USER $DB_USER
+	# wp config DB_PASSWORD $DB_PASSWORD
+	wp config create --dbname=$DB_NAME-dbuser=$DB_USER --dbpass=$DB_PASSWORD
 fi
-mv /var/www/wp-config.php /var/www/wordpress/wp-config.php
+#mv /var/www/wp-config.php /var/www/wordpress/wp-config.php
+
 /usr/sbin/php-fpm7.4 -F
